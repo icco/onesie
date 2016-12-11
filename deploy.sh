fd295840a54b0000abcf51db1f33aa87cb1d885f
Nat Welch <nat@natwelch.com>
2016-12-11 18:36:56 +0000

    debug

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,8 @@ if [ -z "$TRAVIS_TAG" ]; then
   TAG="$TRAVIS_TAG";
 fi
 
+echo "BRANCH: $TRAVIS_BRANCH"
+echo "TAG: $TRAVIS_TAG"
+echo "Final tag: $TAG"
+
 ./packer build -var "account_file=onesie.json" -var "tag=$TAG" packer.json
