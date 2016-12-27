e1a45b973259e84c69af8fd54985a039e72ad937
Nat Welch <nat@natwelch.com>
2016-12-27 06:51:44 +0000

    whoops

--- hitch-install.sh
+++ hitch-install.sh
@@ -3,8 +3,7 @@
 cd /tmp
 git clone https://anonscm.debian.org/git/collab-maint/hitch.git -b jessie-backports
 cd hitch
-sudo aptititude install -y libev-dev libssl-dev automake python-docutils flex bison pkg-config
+sudo aptitude install -y libev-dev libssl-dev automake python-docutils flex bison pkg-config
 ./configure
-ls -al
-automake
+ls -al *
 make
