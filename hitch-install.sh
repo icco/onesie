1d7d96e098ff362e87658cfccf9f26de78ba7a9c
Nat Welch <nat@natwelch.com>
2016-12-27 06:35:25 +0000

    aptitude dpes

--- hitch-install.sh
+++ hitch-install.sh
@@ -3,6 +3,7 @@
 cd /tmp
 git clone https://anonscm.debian.org/git/collab-maint/hitch.git
 cd hitch
+sudo aptititude install -y libev-dev libssl-dev automake python-docutils flex bison pkg-config
 ./bootstrap
 ./configure
 ls -al
